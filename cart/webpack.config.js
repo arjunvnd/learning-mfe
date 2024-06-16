13e4362b4c1479f876cd546af4d6f52fcee5f06c
Arjun V <arjunvnd@gmail.com>
2024-06-16 20:16:58 +0530

    add cart

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -3,17 +3,14 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 module.exports = {
   mode: "development",
   devServer: {
-    port: 8081,
+    port: 8082,
   },
   plugins: [
     new ModuleFederationPlugin({
-      // The name used by container app to identify remote
-      name: "products",
-      // filename is a manifest file. Convention is to keep it as remoteEntry.js
+      name: "cart",
       filename: "remoteEntry.js",
       exposes: {
-        // List of the files I want to expose
-        "./ProductsIndex": "./src/index",
+        "./CartShow": "./src/index",
       },
     }),
     new HtmlWebpackPlugin({
--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,3 +1,4 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -6,6 +7,18 @@ module.exports = {
     port: 8080,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      // name - only needed for remote repos. Added for clarity in container repo
+      name: "container",
+      // remote - list of projects that container looks for
+      remotes: {
+        // import "products/ProductsIndex"; when importing in a file the import statement looks for products inside node_modules
+        // then check if we have defined a product remote key inside module federation
+        // RHS - products has to match the name inside the remote repo
+        products: "products@http://localhost:8081/remoteEntry.js",
+        carts: "cart@http://localhost:8082/remoteEntry.js",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
